8c6b884ed0760117dc624382a8a57d9cbb127c6a
Ilkka Jylhä <ilkka.jylha@iki.fi>
2015-09-15 15:57:52 +0300

    Add arduino-pir code
    Simple code that reads PIR state, prints the status via serial and waits
    500ms

---
+++ arduino-pir/arduino-pir.ino/arduino-pir.ino.ino
@@ -0,0 +1,26 @@
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 3;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED On
+    Serial.println("1");
+    delay(500);
+    }
+   else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    Serial.println("0");
+    delay(500);
+    }
+}
+
