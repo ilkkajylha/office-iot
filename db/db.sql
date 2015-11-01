e4d9a3b22995f699391299d9497c435a8b05d181
Ilkka Jylhä <ilkka.jylha@iki.fi>
2015-11-01 21:37:50 +0200

    add user and companies to database

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,25 @@
-CREATE TABLE `rooms` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `room_number` varchar(20) NOT NULL,
-  `room_status` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf-8;
+CREATE DATABASE IF NOT EXISTS `officeiot` DEFAULT CHARACTER SET utf8;
+USE `officeiot`;
+
+
+CREATE TABLE rooms (
+        id              int NOT NULL AUTO_INCREMENT,
+        name            char(100),
+        company_name    char(100),
+        status          int(1),
+        primary key (id),
+        foreign key (company_name) references companies
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE users (
+        email           char(100) not null,
+        password        char(100),
+        company_name    char(100),
+        primary key (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE companies (
+        id              int NOT NULL AUTO_INCREMENT,
+        company_name    char(100) not null,
+        primary key (id),
+        foreign key (comapny_name) references rooms,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
